982483d14614a6e86c543956033b21952d31f02d
IselinLulu <65400419+IselinLulu@users.noreply.github.com>
2020-10-14 20:14:47 +0200

    Create level-1.js

---
+++ level-1.js
@@ -0,0 +1,45 @@
+// Question 1
+
+var myName = "Iselin";
+
+
+// Question 2
+
+var person = {
+  eyecolor: "blue",
+  haircolor: "blonde",
+  };
+  
+  
+  // Question 3
+  
+  var outOfStock = "false";
+  
+  if (outOfStock === true){
+    console.log("Out of stock");
+ }else (outOfStock === false){
+    console.log("In stock");
+ }
+ 
+ 
+ // Question 4
+ 
+ var numbers = ["one","two","three","four","five"];
+ 
+ console.loh(numbers.length);
+ 
+ 
+ 
+ //Question 5
+ 
+for (var i=15; i < 26; i++){ 
+  console.log([i]);
+}
+
+
+//Question 6
+
+
+ 
+ 
+   
